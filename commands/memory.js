9d9601ee063e74a6bc51be903a0c8851647bab74
lennysgarage <lennysgarage1@gmail.com>
2021-07-29 22:12:02 -0400

    Added an optional date parameter to specify when to look for a message

--- commands/memory.js
+++ commands/memory.js
@@ -1,13 +1,11 @@
 require('../utils/ExtendedMessage'); /* for inline reply */
-const discordEpochPlusOneYear = 1451627326000;
-/* 1420070400000 + 31556926000*/
 const { grabChannel } = require('../utils/grabChannel');
 const { shift } = require('../utils/snowflakeUtil');
 
 module.exports = {
     name: 'memory',
     description: 'Grab a memory from the past',
-    usage: '[channel]',
+    usage: '[channel] [date|numOfYears]',
     execute(message, args) {
         let messages = message.channel.messages;
         /* Can specify channel by name or id */
@@ -15,13 +13,21 @@ module.exports = {
             messages = grabChannel(message, args[0]);
             if (messages === undefined) return;
         }
+
+        let numOfYears = 1; // Default behaviour
+        /* Can specify when to find a memory from */
+        /* Can enter a date or just how long ago */
+        if (args[1]) {
+            if (!isNaN(args[1])) numOfYears = parseFloat(args[1]);
+            else numOfYears = ((new Date().getTime() - new Date(args[1]).getTime())/31556926000).toFixed(2);
+        }
                 
         /* Here we are trying to make an artifical discord snowflake.
          * The timestamp in a snowflake is (snowflake >> 22) + 1420070400000
-         * Since we are trying to create a snowflake from a year ago we need to grab the (current time) - discordEpochPlusOneYear
+         * Since we are trying to create a snowflake from a year ago we need to grab the (current time) - discordEpoch + # of years
          * Bit shifting this to the right 22 times gives us an artifical snowflake that only has a proper timestamp
         /* For deeper information on snowflakes: https://discord.com/developers/docs/reference#snowflakes */
-        let dateAfter = shift((message.createdTimestamp - discordEpochPlusOneYear), 22);
+        let dateAfter = shift((message.createdTimestamp - 1420070400000 - (31556926000 * numOfYears)), 22);
         
 
         messages.fetch({
@@ -29,7 +35,8 @@ module.exports = {
              after: dateAfter
             })
         .then(collectionOfMessages => {collectionOfMessages
-            const responseMsg = "Hey checkout this memory from a year ago!"
+            const responseMsg = numOfYears > 1 ? `Hey checkout this memory from ${numOfYears} years ago!` : 
+            "Hey checkout this memory from a year ago!";
             // Cannot inline reply to message in a different channel (discord limitation atm)
             if(args[0]) {
                 message.channel.send({

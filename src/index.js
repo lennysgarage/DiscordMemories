b8f7b215552086aae5288d3e171579f77375b21b
lennysgarage <lennysgarage1@gmail.com>
2021-08-29 21:01:21 -0400

    feat: get members

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const { prefix, token } = require('./config.json');
 
 
 const client = new Client({ 
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES],
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES],
     repliedUser: false 
 });
 client.commands = new Collection();
@@ -17,8 +18,20 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
     client.user.setActivity(`${prefix}help`, { type: 'WATCHING' });
-    console.log(`Currently servicing ${client.guilds.cache.size} guilds`);
+    console.log(`Starting to service ${client.guilds.cache.size} guilds`);
     console.log('Ready!');
+
+
+    setInterval(async () => {
+        // Simplistic get members, includes bot accounts
+        await client.guilds.fetch();
+        let memberCount = 0;
+        for (const guild of client.guilds.cache.values()) {
+            memberCount += guild.memberCount;
+        }
+            
+        console.log(`Currently servicing ${client.guilds.cache.size} guilds & ${memberCount} users`);
+    }, 900000);
 });
 
 

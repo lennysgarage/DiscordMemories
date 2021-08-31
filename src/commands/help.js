333ebe2cfcf2e46f5d113ed9f162f311f06771bb
lennysgarage <lennysgarage1@gmail.com>
2021-08-31 12:58:47 -0400

    feat: embed help message

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,45 +1,31 @@
-const { prefix } = require('../config.json')
+const { prefix } = require('../config.json');
+const { messageEmbed, MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'help',
     description: 'List of all commands',
     aliases: ['commands'],
     execute(message, args) {
-        const data = [];
-        const { commands } = message.client;
-
-        if (!args.length) {
-            data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join(', '));
-            data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
-        
-            return message.author.send({content: data.join('\n')})
-                .then(() => {
-                    if (message.channel.type === 'dm') return;
-                    message.reply({ content: 'I\'ve sent you a DM with all my commands!' });
-                })
-                .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.reply({ content: 'it seems like I can\'t DM you! Do you have DMs disabled?' });
-                });
-        }
-
-
-        const name = args[0].toLowerCase();
-        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
-        if (!command) {
-            return message.reply({ content: 'that\'s not a valid command!', allowedMentions: { repliedUser: false } });
-        }
-
-        data.push(`**Name:** ${command.name}`);
-
-        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-        if (command.description) data.push(`**Description:** ${command.description}`);
-        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-
-        message.channel.send({ content: data.join('\n') });
-
+        const helpEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setAuthor('Help Menu', 'https://cdn.discordapp.com/avatars/867469054931501078/bf64ca402609e37b2db3f9395a652abd.webp', 'https://github.com/lennysgarage/DiscordMemories')
+        .setThumbnail('https://cdn.discordapp.com/avatars/867469054931501078/bf64ca402609e37b2db3f9395a652abd.webp')
+        .addFields(
+            { name: 'Memory Command', value: '\u200b' },
+            { name: '```memory [channel|ID] [date|numOfYears]```', value: '- Showcases a memory from any user' },
+            { name: '```memory```', value: '- Showcases a memory from a year ago' },
+            { name: '```memory general```', value: '- Showcases a memory from the general channel a year ago' },
+            { name: '```memory general 2019-05-05```', value: '- Showcases a memory from general around May 5th, 2019' },
+            { name: '```memory general 3.14159```', value: '- Showcases a memory from general ~pi years ago' },
+            { name: '\u200b', value: '\u200B' },
+            { name: 'Random Command', value: '\u200b'},
+            { name: '```random [channel|ID]```', value: '- Showcases a random memory from any user' },
+            { name: '```random```', value: '- Showcases a random memory' },
+            { name: '```random general```', value: '- Showcases a random memory from general' },
+        )
+        .setTimestamp()
+        .setFooter('Prefix: ==', 'https://cdn.discordapp.com/avatars/867469054931501078/bf64ca402609e37b2db3f9395a652abd.webp');
+            
+        message.channel.send({ embeds: [helpEmbed] });
     }
 }

7fd88f871bca1c63c0b8e697326a0615e3032dde
lennysgarage <lennysgarage1@gmail.com>
2021-09-14 23:14:53 -0400

    feat: interact with bot via prefix or mention

--- src/index.js
+++ src/index.js
@@ -54,34 +54,32 @@ client.on('guildDelete', guild => {
 
 
 client.on('messageCreate', async message => {
-    /*Bring up help menu if mentioned */
-    if (!message.author.bot && !message.mentions.everyone && message.mentions.has(client.user)) {
+    /* Interact via prefix or mention */
+    if (!message.author.bot && !message.mentions.everyone 
+        && (message.mentions.has(client.user) || message.content.startsWith(prefix))) {
+        /* Command from prefix */
+        let args;
+        if (message.content.startsWith(prefix)) 
+            args = message.content.slice(prefix.length).trim().split(/ +/);
+        else /* Command from mention */
+            args = message.content.slice(client.user.id.length +4).trim().split(/ +/); 
+            // +4 accounts for <@!>
+
+        // Don't want to fire off when just prefix
+        if (!message.mentions.has(client.user) && args[0] == '') return; 
+        const commandName = args.shift().toLowerCase() || 'help'; // Default to help menu
+
+        if (!client.commands.has(commandName)) return; // Check if cmd exists
+        const command = client.commands.get(commandName);
+
+        /* Execute the command */
         try { 
-            client.commands.get('help').execute(message, []);
+            command.execute(message, args);
         } catch(error) {
             console.error(error);
             await message.reply({ content: 'there was an error trying to bring up the help menu!', allowedMentions: { repliedUser: true } });    
         }
-        return;
     }
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-
-    if (!client.commands.has(commandName)) return; // Check if cmd exists
-
-    const command = client.commands.get(commandName);
-
-
-    /* Execute the command */
-    try {
-        command.execute(message, args);
-    } catch (error) {
-        console.error(error);
-        await message.reply({ content: 'there was an error trying to execute that command!', allowedMentions: { repliedUser: true } });
-    }
-
 });
 
 /* Default way to start bot 

db60d0bd5ef092940a2ab11e8f8b17fdfb3a0720
lennysgarage <lennysgarage1@gmail.com>
2022-01-13 20:45:42 -0500

    feat: slashcommandof invite command

--- src/slashcommands/invite.js
+++ src/slashcommands/invite.js
@@ -1,20 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require("discord.js");
 const { prefix, inviteLink } = require('../config.json');
 
 module.exports = {
-    name: 'invite',
-    description: 'Invite link for Memories Bot!',
-    usage: [''],
-    usage_desc: ['Invite link for bot!'],
-    async execute(message, args) {
+    data: new SlashCommandBuilder()
+        .setName('invite')
+        .setDescription('Invite link for Memories Bot!'),
+    async execute(interaction) {
         const inviteEmbed = new MessageEmbed()
         .setColor('#0099ff')
-        .setAuthor('Invite Bot', message.client.user.avatarURL(), inviteLink)
+        .setAuthor('Invite Bot', interaction.client.user.avatarURL(), inviteLink)
         .addFields(
             { name: 'Click Below', value: `[Invite Me!](${inviteLink})` })
         .setTimestamp()
         .setFooter(`Prefix: ${prefix}`);
 
-        return await message.channel.send({ embeds: [inviteEmbed] });
+        return await interaction.reply({ embeds: [inviteEmbed] });
     }
 }

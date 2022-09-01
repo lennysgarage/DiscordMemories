c3ff8bbb87eded27e2e615b3ba284db692bd227c
lennysgarage <lennysgarage1@gmail.com>
2022-08-31 21:06:52 -0400

    fix: change all checkout to check out -  2.7.1
    fix: change all ooccurrences of checkout to check out
    
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- src/commands/memory.js
+++ src/commands/memory.js
@@ -50,8 +50,8 @@ module.exports = {
             /* Here we check for the actual timestamp of the msg */
             const msgToSend = collectionOfMessages.random();
             const timeOfMsg = ((new Date().getTime() - msgToSend.createdTimestamp)/31535900000).toFixed(8);
-            const responseMsg = (timeOfMsg > 1 || timeOfMsg < 1) ? `Hey checkout this memory from ${timeOfMsg} years ago!` : 
-            "Hey checkout this memory from a year ago!";
+            const responseMsg = (timeOfMsg > 1 || timeOfMsg < 1) ? `Hey check out this memory from ${timeOfMsg} years ago!` : 
+            "Hey check out this memory from a year ago!";
             if (args[0]) {
                 const embed = new MessageEmbed().setTitle(responseMsg).setURL(msgToSend.url);
                 await message.channel.send({ embeds: [embed] });
--- src/slashcommands/memory.js
+++ src/slashcommands/memory.js
@@ -19,7 +19,7 @@ module.exports = {
         let messages = interaction.channel.messages;
         const channel = interaction.options.get('channel');
         const time = interaction.options.get('date');
-        
+
         if (channel !== null) {
             if (channel.channel.type !== 'GUILD_TEXT') {
                 return await interaction.reply({ content: "Channel must be a text channel!", ephemeral: true });
@@ -32,13 +32,13 @@ module.exports = {
         /* Can enter a date or just how long ago */
         if (time !== null) {
             if (!isNaN(time.value)) numOfYears = parseFloat(time.value).toFixed(8);
-            else numOfYears = ((interaction.createdTimestamp - (interaction.createdTimestamp % 86400000) - new Date(time.value).getTime())/31535900000).toFixed(8);
+            else numOfYears = ((interaction.createdTimestamp - (interaction.createdTimestamp % 86400000) - new Date(time.value).getTime()) / 31535900000).toFixed(8);
             // 8 Digits allow for better accuracy when dealing with miliseconds
             // We grab the current time subtracting the number of miliseconds currently in the day to get the earliest moment
             // We take this time divided by roughly how many miliseconds in a year
         }
 
-        
+
         /* Here we are trying to make an artifical discord snowflake.
          * (timestamp_ms - DISCORD_EPOCH) << 22 (timestamp to snowflake)
          * Since we are trying to create a snowflake from possibly multiple years ago we use the biggest unit, a year
@@ -49,7 +49,7 @@ module.exports = {
         /* For deeper information on snowflakes: https://discord.com/developers/docs/reference#snowflakes */
         let dateAfter = shift((interaction.createdTimestamp - (interaction.createdTimestamp % 86400000) - (DISCORD_EPOCH - 18000000) - (31535900000 * numOfYears)), 22);
         let todayPlusOne = shift(interaction.createdTimestamp - (interaction.createdTimestamp % 86400000) + 86400000 - DISCORD_EPOCH, 22);
-        if (numOfYears < 0 || dateAfter < 0 || dateAfter > todayPlusOne || isNaN(dateAfter) ) {
+        if (numOfYears < 0 || dateAfter < 0 || dateAfter > todayPlusOne || isNaN(dateAfter)) {
             return await interaction.reply({ content: "Invalid date", ephemeral: true });
         }
 
@@ -58,12 +58,12 @@ module.exports = {
             const collectionOfMessages = await messages.fetch({ limit: 10, after: dateAfter });
             /* Here we check for the actual timestamp of the msg */
             const msgToSend = collectionOfMessages.random();
-            const timeOfMsg = ((new Date().getTime() - msgToSend.createdTimestamp)/31535900000).toFixed(8);
-            const responseMsg = (timeOfMsg > 1 || timeOfMsg < 1) ? `Hey checkout this memory from ${timeOfMsg} years ago!` : 
-            "Hey checkout this memory from a year ago!";
+            const timeOfMsg = ((new Date().getTime() - msgToSend.createdTimestamp) / 31535900000).toFixed(8);
+            const responseMsg = (timeOfMsg > 1 || timeOfMsg < 1) ? `Hey check out this memory from ${timeOfMsg} years ago!` :
+                "Hey check out this memory from a year ago!";
             const embed = new MessageEmbed().setTitle(responseMsg).setURL(msgToSend.url);
             await interaction.reply({ embeds: [embed] });
-        } catch(err) {
+        } catch (err) {
             if (err.code == 50001) {
                 return await interaction.reply({ content: "I seem to be missing access to view this channel!", ephemeral: true });
             }

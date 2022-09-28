751579787aafac6e7519692b754492645e83599f
lennysgarage <lennysgarage1@gmail.com>
2022-09-28 00:03:33 -0400

    feat: update info command to defer fetching analytics info
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- src/slashcommands/info.js
+++ src/slashcommands/info.js
@@ -14,8 +14,71 @@ module.exports = {
             memberCount += guild.memberCount;
         }
 
+        let infoEmbed = {
+            color: '#0099ff',
+            title: 'Information:',
+            thumbnail: {
+                url: interaction.client.user.avatarURL(),
+            },
+            fields: [
+                {
+                    name: '❯ Info:',
+                    value: `
+• Name: <@${interaction.client.user.id}> \`(${interaction.client.user.tag})\` 
+• ID: \`${interaction.client.user.id}\`
+• Creation Date: \`${interaction.client.user.createdAt}\`
+• Prefix: \`${prefix}\`
+• Discord.js: \`${pkg.dependencies['discord.js']}\`
+• Node: \`${process.version}\` 
+                    `,
+                },
+                {
+                    name: '\u200b',
+                    value: '\u200b',
+                },
+                {
+                    name: '❯ Bot:',
+                    value: `
+• Bot Latency: \`${Date.now() - interaction.createdTimestamp} ms\`
+• Uptime: \`${new Date(process.uptime() * 1000).toISOString().substr(11, 8)}\`
+• Ram: \`${(process.memoryUsage.rss() * 0.000001).toFixed(2)} MB\`
+• Version: \`${pkg.version}\`
+                    `,
+                    inline: true,
+                },
+                {
+                    name: '❯ Analytics:',
+                    value: `
+• Users: \`Loading...\`
+• Channels: \`Loading...\`
+• Servers: \`Loading...\`
+                    `,
+                    inline: true,
+                },
+                {
+                    name: '\u200b',
+                    value: '\u200b',
+                },
+                {
+                    name: '❯ Other:',
+                    value: `
+• Developer: <@108038507708141568> lennysgarage#2361
+• Invite: [Click Here](${inviteLink})
+• Vote: [Click Here](${voteLink})
+• Support Server: [Click Here](${supportServer})
+                    `
+                }
+            ],
+        };
 
-        const infoEmbed = {
+        try {
+            await interaction.reply({ embeds: [infoEmbed] });
+        } catch (err) {
+            console.log("Failed to send embed", err)
+        }
+
+        // Update embed with Analytics information.
+        infoEmbed = {
             color: '#0099ff',
             title: 'Information:',
             thumbnail: {
@@ -71,11 +134,10 @@ module.exports = {
                 }
             ],
         };
-
         try {
-            await interaction.reply({ embeds: [infoEmbed] });
+            await interaction.editReply({ embeds: [infoEmbed] });
         } catch (err) {
-            console.log("Failed to send embed", err)
+            console.log("Failed to update reply", err)
         }
     },
 };

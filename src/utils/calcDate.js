91d53a67f0ae9fbe943005ea93160422d72bb32e
lennysgarage <lennysgarage1@gmail.com>
2023-02-02 22:18:20 -0500

    feat: calculate only year and days
    Signed-off-by: lennysgarage <lennysgarage1@gmail.com>

--- src/utils/calcDate.js
+++ src/utils/calcDate.js
@@ -1,14 +1,26 @@
 
 module.exports = {
     calcDate(date) {
-        let diffDate = new Date(new Date().getTime() - date);
-        let years = diffDate.toISOString().slice(0, 4) - 1970;
+        let difference = dateDiffInDays(new Date(date), new Date());
+        let years = Math.floor(difference / 365.25);
+        let days = Math.floor(difference % 365.25); 
 
         if (years > 0) {
-            return years + " Years " + diffDate.getMonth() + " Months " 
-            + (diffDate.getDate() - 1) + " Days ago!";
+            return years + " Years " + days + " Days ago!";
         } else {
-            return diffDate.getMonth() + " Months " + (diffDate.getDate() - 1) + " Days ago!";
+            return days + " Days ago!";
         }
     }
 }
+
+// Source: https://stackoverflow.com/a/15289883
+function dateDiffInDays(a, b) {
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+    // Discard the time and time-zone information.
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+  
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+  }
+  
+  
